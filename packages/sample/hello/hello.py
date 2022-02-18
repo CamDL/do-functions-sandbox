a61230c67600dffa2d89969913f158369ebadd01
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 03:18:01 -0500

    return a body

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,7 +1,7 @@
+import requests
+
 def main(args):
       cat = args.get("cat", "all")
       payload = {'category':cat}
-      #response = requests.get('https://inshortsapi.vercel.app/news',params=payload)
-      text = f'Hello {cat}'
-      return text
-  
+      response = requests.get('https://inshortsapi.vercel.app/news',params=payload)
+      return {"body": response.json()}
