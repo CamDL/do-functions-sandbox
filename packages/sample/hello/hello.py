408e8c6e711bb9499225afeed5915e9d2c20b6d2
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 03:08:37 -0500

    text

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -2,5 +2,6 @@ def main(args):
       cat = args.get("cat", "all")
       payload = {'category':cat}
       #response = requests.get('https://inshortsapi.vercel.app/news',params=payload)
-      return cat
+      text = f'Hello {cat}'
+      return text
   
